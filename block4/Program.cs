6f9c654d1311271614cf88435a73f2c2a341175b
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-04-22 23:02:05 +0300

    у цьому блоці нам потрібно було написати калькулятор який приймає ввід у форматі n x де n - номер функції, а x - аргумент функції. для цього було використано вбудований делегат Func<int, double>, в який передаю лямбда вирази для x, які описують функції. далі я роблю масив цих функції. наступним кроком в нескінченному циклі приймаю аргументи n - номер функції та x - аргумент, викликаю n-ту функцію з аргументом x та друкую результат.

---
+++ block4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Func<double, double>[] funcs = new Func<double, double>[3];
+
+    funcs[0] = x => Math.Sqrt(Math.Abs(x));
+    funcs[1] = x => Math.Pow(x, 3);
+    funcs[2] = x => Math.Sin(x);
+
+    Console.WriteLine("Формат вводу: \"F X\" де F - функція, X - її аргумент:");
+    Console.WriteLine("Підтримувані функції: 0 - sqrt(abs(x))");
+    Console.WriteLine("                      1 - x^3");
+    Console.WriteLine("                      2 - sin(x)");
+
+    while (true)
+    {
+      try
+      {
+        string[] parts = Console.ReadLine().Trim().Split();
+        int n = Convert.ToInt32(parts[0]);
+        double x = Convert.ToDouble(parts[1]);
+
+        Func<double, double> targetFunc = funcs[n];
+
+        Console.WriteLine(targetFunc(x));
+      }
+      catch
+      {
+        Console.WriteLine("Yнеправильний ввід тому я зупиняю програму");
+        break;
+      }
+    }
+  }
+}
