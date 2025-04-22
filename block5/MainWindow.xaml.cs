1e489fa2f5bb1586f7d767b9cc4ec1943e48d336
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-04-22 23:30:32 +0300

    простий wpf-додаток, 3 чекбокси, 4 кнопки.
    1) змінює колір через властивіть вікна
    
    2) змінює прозорість через властивіть вікна (через особливість wpf, довелося зробити кастомну верхню панель з кнопкою закриття програми)
    
    3) виводить алерт-бокс з привітанням
    
    якщо чекбокс натиснутий (подія спрацювала), на цю кнопку вішаємо дію
    
    Суперкнопка: на ній висить обробник подій RoutedEventHandler, який оглядає які події (RoutedEventArgs) відбулися (чекбокси натиснуті). і прив'язує їх до кнопки (superButton.Click += handler) або відв'язує (superButton.Click -= handler)

---
+++ block5/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace block5;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click_Color(object sender, RoutedEventArgs e)
+    {
+        if (Background == Brushes.Crimson)
+        {
+            Background = Brushes.Yellow;
+        }
+        else
+        {
+            Background = Brushes.Crimson;
+        }
+    }
+
+    private void Button_Click_Opacity(object sender, RoutedEventArgs e)
+    {
+        if (Opacity > 0.7)
+        {
+            Opacity -= 0.3;
+        }
+        else
+        {
+            Opacity += 0.3;
+        }
+    }
+
+    private void Button_Click_Hello(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Привіт!", "Привітання", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+    }
+
+    private void CheckBox_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is CheckBox checkBox)
+        {
+            RoutedEventHandler handler = checkBox.Name switch
+            {
+                "checkColor" => Button_Click_Color,
+                "checkOpacity" => Button_Click_Opacity,
+                "checkHello" => Button_Click_Hello,
+                _ => null
+            };
+
+            if (handler != null)
+            {
+                if (checkBox.IsChecked == true)
+                {
+                    superButton.Click += handler;
+                }
+                else
+                {
+                    superButton.Click -= handler;
+                }
+            }
+        }
+    }
+
+    private void Button_Click_Super(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я супер-кнопка і у мене цього не віднімеш;)");
+    }
+
+    private void DragBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void Button_CloseWindow(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
