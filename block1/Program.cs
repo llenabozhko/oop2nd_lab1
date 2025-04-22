7000268bb29ef9e129058bd7fad6708770c09cb4
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-04-22 22:16:28 +0300

    у цьому блоці нам потрібно було написати клас Timer який приймає в себе метод та через який час його потрібно повторювати. щоб передати метод в конструктор нашого таймеру ми використали делегат . Кожен таймер створюється в окремому потоці, при зупинці таймера цей поток завершується. замість кастомного делегата можна використати .

---
+++ block1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Timer timer1 = new(CustomMethod1, 1000);
+    Timer timer2 = new(CustomMethod2, 500);
+    Console.ReadLine();
+    timer1.Stop();
+
+    Console.ReadLine();
+    timer2.Stop();
+  }
+  public static void CustomMethod1()
+  {
+    Console.WriteLine("Hello, world!");
+  }
+  public static void CustomMethod2()
+  {
+    Console.WriteLine("Hello, kitty!");
+
+  }
+
+
+}
+
+public class Timer
+{
+  private int _time;
+  public delegate void Method();
+  private bool isActive;
+  private Thread timerThread;
+
+  public Timer(Method method, int time)
+  {
+    _time = time;
+    isActive = true;
+    timerThread = new Thread(() => Start(method));
+    timerThread.IsBackground = true;
+    timerThread.Start();
+  }
+
+  private void Start(Method method)
+  {
+    while (isActive)
+    {
+      method();
+      Thread.Sleep(_time);
+    }
+  }
+
+  public void Stop()
+  {
+    isActive = false;
+    if (timerThread.IsAlive)
+    {
+      timerThread.Join();
+    }
+  }
+}
