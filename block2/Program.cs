262410438ade0d6b05c15a3a45085a2b7c9aa2d8
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-04-22 22:27:28 +0300

    у цьому блоці нам потрібно було відфільтрувати масив цілих чисел, за параметром, заданим в лямбда-виразі, що призначений делегату delegate bool Filter(int n); замість якого можна використати Func<int, bool>. цей делегат передається у два методи:
    1) фільтрація за допомогою LINQ, ключове слово Where. якщо передати число в метод фільтрації і він поверне true - ми його залишаємо. якщо false - відкидаємо.
    
    2) ручна фільтрація, аналогічна, але написана руцями.

---
+++ block2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+  delegate bool Filter(int n);
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Введіть масив через пробіли:");
+    // int[] arr = Array.ConvertAll(Console.ReadLine().Trim().Split(), Convert.ToInt32);
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    Console.Write("Введіть дільник: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+
+    Filter filterMultiple = x => x % k == 0;
+
+    Console.WriteLine($"Фільтрування за допомогою LINQ: {string.Join(", ", FilterUsingLinq(arr, filterMultiple))}");
+    Console.WriteLine($"Ручне фільтрування: {string.Join(", ", FilterManually(arr, filterMultiple))}");
+    FilterUsingLinq(arr, filterMultiple);
+    FilterManually(arr, filterMultiple);
+  }
+
+  static int[] FilterUsingLinq(int[] arr, Filter filter)
+  {
+    return arr.Where((x) => filter(x)).ToArray();
+  }
+
+  static int[] FilterManually(int[] arr, Filter filter)
+  {
+    int length = 0;
+    foreach (int num in arr)
+      if (filter(num))
+        length++;
+
+    int[] filteredArr = new int[length];
+    int j = 0;
+    foreach (int num in arr)
+    {
+      if (filter(num))
+        filteredArr[j++] = num;
+    }
+
+    return filteredArr;
+  }
+
+}
